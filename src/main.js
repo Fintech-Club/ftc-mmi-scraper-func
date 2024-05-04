989af6fb2fcb521a9ade9cc46da95cabfcf14eff
Romit  Dey <romit77dey@gmail.com>
2024-05-05 01:36:46 +0530

    puppeteer added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { Client, Databases } from "node-appwrite";
+import puppeteer from "puppeteer";
 
 async function updateMMI(log, databases, DOCUMENT_ID, score) {
   const res = await databases.updateDocument(
@@ -11,16 +12,51 @@ async function updateMMI(log, databases, DOCUMENT_ID, score) {
   if (DOCUMENT_ID == res.$id) log("[MMI UPDATED] ID: " + res.$id);
 }
 
+async function updateMMIFromWeb(log, databases, DOCUMENT_ID, url) {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    await page.waitForSelector(".market-fng-gauge__dial-number-value");
+
+    const mmiText = await page.$eval(
+      ".market-fng-gauge__dial-number-value",
+      (element) => element.textContent.trim()
+    );
+
+    const mmi = parseFloat(mmiText);
+
+    await updateMMI(log, databases, DOCUMENT_ID, mmi);
+    console.log(mmiText);
+    console.log(mmi);
+
+    await browser.close();
+  } catch (error) {
+    log("Error updating MMI from web:", error);
+  }
+}
+
 async function update_CNN_MMI(log, databases) {
   let score = Math.floor(Math.random() * 101);
 
-  await updateMMI(log, databases, process.env.CNN_DOCUMENT_ID, score);
+  await updateMMIFromWeb(
+    log,
+    databases,
+    process.env.CNN_DOCUMENT_ID,
+    "https://edition.cnn.com/markets/fear-and-greed"
+  );
 }
 
 async function update_TICKERTAPE_MMI(log, databases) {
   let score = Math.floor(Math.random() * 101);
 
-  await updateMMI(log, databases, process.env.TICKERTAPE_DOCUMENT_ID, score);
+  await updateMMIFromWeb(
+    log,
+    databases,
+    process.env.TICKERTAPE_DOCUMENT_ID,
+    "https://www.tickertape.in/market-mood-index"
+  );
 }
 
 // This is your Appwrite function
