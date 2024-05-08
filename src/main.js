3372740ee090de09464a26a2900d07bca47d4797
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2024-05-09 02:21:28 +0530

    moved all the code to a single file

--- src/main.js
+++ src/main.js
@@ -1,12 +1,68 @@
 import { Client, Databases } from "node-appwrite";
-import { getCNNMMI, getTickerTapeMMI } from "./scrape-mmi";
+
+// SCRAPE CODE
+
+import axios from "axios";
+import { load } from "cheerio";
+
+async function scrapePage(url, selector) {
+  try {
+    // Fetch the HTML content of the page
+    const response = await axios.get(url);
+    const html = response.data;
+
+    // Parse the HTML
+    const $ = load(html);
+
+    // Find and extract the content you want
+    const content = $(selector).text().trim();
+
+    return content;
+  } catch (error) {
+    console.error("Error fetching or parsing page:", error);
+    return null;
+  }
+}
+
+export async function getTickerTapeMMI(log) {
+  try {
+    const mmi = await scrapePage(
+      "https://www.tickertape.in/market-mood-index",
+      "div.mmi-value > span.number"
+    );
+
+    return parseInt(mmi);
+  } catch (error) {
+    console.error("Error fetching or parsing page:", error);
+    return null;
+  }
+}
+
+export async function getCNNMMI(log) {
+  try {
+    const mmi = await scrapePage(
+      "https://edition.cnn.com/markets/fear-and-greed",
+      "div.market-fng-gauge__dial > span.market-fng-gauge__dial-number-value"
+    );
+
+    return parseInt(mmi);
+  } catch (error) {
+    console.error("Error fetching or parsing page:", error);
+    return null;
+  }
+}
+
+// =================================================================================================
+// =================================================================================================
+// =================================================================================================
+// =================================================================================================
 
 async function updateMMI(log, databases, DOCUMENT_ID, score) {
   const res = await databases.updateDocument(
     process.env.DATABASE_ID,
     process.env.COLLECTION_ID,
     DOCUMENT_ID,
-    { score: score },
+    { score: score }
   );
 
   if (DOCUMENT_ID == res.$id) log("[MMI UPDATED] ID: " + res.$id);
